19783065dcb24cf1eb9aa80ae90c25254c2b6d45
goosefin <agustinafin96@gmail.com>
2023-09-05 20:33:52 -0400

    Created handler functions for cart

--- Section 12 - Food order app/src/components/Meals/MealItem/MealItemForm.js
+++ Section 12 - Food order app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,27 @@
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
+import { useRef, useState } from "react";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredAmount = amountInputRef.current.defaultValue;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (enteredAmount.trim() === 0 || enteredAmount < 1 || enteredAmount > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +33,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
