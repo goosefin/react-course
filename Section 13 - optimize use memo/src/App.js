85c400c4f920f5d100b1abbf3fd173a9c12242a0
goosefin <agustinafin96@gmail.com>
2023-09-13 19:33:07 -0400

    used useMemo()

--- Section 13 - optimize use memo/src/App.js
+++ Section 13 - optimize use memo/src/App.js
@@ -1,19 +1,21 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useMemo } from "react";
 
-import './App.css';
-import DemoList from './components/Demo/DemoList';
-import Button from './components/UI/Button/Button';
+import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  const [listTitle, setListTitle] = useState('My List');
+  const [listTitle, setListTitle] = useState("My List");
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
+    setListTitle("New Title");
   }, []);
 
   return (
-    <div className='app'>
-      <DemoList title={listTitle} items={[5, 3, 1, 10, 9]} />
+    <div className="app">
+      <DemoList title={listTitle} items={listItems} />
       <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
