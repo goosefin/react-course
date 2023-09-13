a911e49a3baef882bd7f55fba50c9e96543c35fe
goosefin <agustinafin96@gmail.com>
2023-09-12 20:15:51 -0400

    UseEffect

--- Section 13 - How React Works/src/App.js
+++ Section 13 - How React Works/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import "./App.css";
 import DemoOutput from "./components/demo/DemoOutput";
@@ -9,9 +9,9 @@ function App() {
 
   console.log("App running");
 
-  const toggle = (e) => {
+  const toggle = useCallback((e) => {
     setShowP((prevShow) => !prevShow);
-  };
+  }, []);
 
   return (
     <div className="app">
