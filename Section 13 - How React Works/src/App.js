90cf5f142d55c3769229da25f2e2692075eb873e
goosefin <agustinafin96@gmail.com>
2023-09-13 19:09:56 -0400

    Updated use Callback with correct dependencies

--- Section 13 - How React Works/src/App.js
+++ Section 13 - How React Works/src/App.js
@@ -6,17 +6,28 @@ import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showP, setShowP] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("App running");
 
-  const toggle = useCallback((e) => {
-    setShowP((prevShow) => !prevShow);
-  }, []);
+  const toggle = useCallback(
+    (e) => {
+      if (allowToggle) {
+        setShowP((prevShow) => !prevShow);
+      }
+    },
+    [allowToggle]
+  );
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showP} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggle}>Toggle Paragraph</Button>
     </div>
   );
