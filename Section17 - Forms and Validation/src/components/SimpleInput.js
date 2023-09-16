311843e964b8df1510dff3e67aa631f10f75c921
goosefin <agustinafin96@gmail.com>
2023-09-15 22:46:09 -0400

    Created form validation with a was touched state

--- Section17 - Forms and Validation/src/components/SimpleInput.js
+++ Section17 - Forms and Validation/src/components/SimpleInput.js
@@ -1,9 +1,56 @@
+import { useRef, useState, useEffect } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name input is valid");
+    }
+  }, [enteredNameIsValid]);
+
+  const nameInputchangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    const enteredValue = nameInputRef.current.value;
+    setEnteredNameIsValid(true);
+    setEnteredName("");
+  };
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitForm}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputchangeHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
